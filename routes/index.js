d3ae7b2c34c39844bf7beecbc3416a8f1cc3459e
Pavel Kostenko <poul.kg@gmail.com>
2013-07-17 23:22:54 +0600

    * added styles.less file, which should be compiled to style.css * changed design of the video list page

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,33 @@
-
 /*
  * GET home page.
  */
 var fs = require('fs');
 
-exports.index = function(req, res){
-  var videosDir = req.app.get('videosDir'),
-      files,
-      mp4files = [],
-      i;
+exports.index = function (req, res) {
+    var videosDir = req.app.get('videosDir'),
+        files,
+        mp4files = [],
+        i,
+        fileName,
+        title;
 
     console.log(videosDir);
-  files = fs.readdirSync(videosDir);
+    files = fs.readdirSync(videosDir);
 
-  // Filter out only mp4 files
-  for (i = 0; i < files.length; i += 1) {
-      if (files[i].match(/\.mp4$/)) {
-          mp4files.push(files[i]);
-      }
-  }
+    // Filter out only mp4 files
+    for (i = 0; i < files.length; i += 1) {
+        if (files[i].match(/\.mp4$/i)) {
+            fileName = files[i];
+            // change dots to spaces, remote mp4
+            title = fileName.replace(/\./g, ' ')
+                .replace(/mp4/gi, '');
 
-  res.render('index', { title: 'Express', mp4files: mp4files });
+            mp4files.push({
+                title: title,
+                fileName: files[i]
+            });
+        }
+    }
+
+    res.render('index', { title: 'Express', mp4files: mp4files });
 };
